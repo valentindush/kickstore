04212bb3f8166234ce3ba62cc99333de287d2065
ishimwe-emma <ishimweemmanuel2005@gmail.com>
2022-10-25 12:13:10 +0200

    added the featured products

--- src/components/FeaturedPro.tsx
+++ src/components/FeaturedPro.tsx
@@ -1,34 +1,74 @@
 import React from "react";
-import shoe1 from '../assets/image 1.png';
-import addToCartIcon from '../assets/Group.svg'
-import filledStar from '../assets/Star 4.svg';
-import unFilledStar from '../assets/Star 5.svg';
+import shoe1 from "../assets/image 1.png";
+import addToCartIcon from "../assets/Group.svg";
+import filledStar from "../assets/Star 4.svg";
+import unFilledStar from "../assets/Star 5.svg";
+import type { FC } from "react";
 
 interface FeaturedPro {
-  rating: string;
+  rating: number;
   description: string;
-  price: string;
+  price: Number;
+  imgSource: string;
+  id: string;
+  indexId: number;
 }
 
-const FeaturedPro = () => {
-  return <div className="w-screen h-[20rem] flex flex-col items-center justify-center gap-[1rem]">
-    <div className="w-full h-auto relative flex items-center justify-center">
-        <div className="w-[7rem] h-[7rem] rounded-full absolute top-[-1.5rem] right-[9rem]" style={{ background: 'linear-gradient(270deg, #FE7831 0%, #FFDEBE 100%)'}}></div>
-        <img src={shoe1} alt="" className="h-[10rem] z-0" />
-        <div className="w-[2rem] h-[2rem] rounded-full grid place-items-center absolute bottom-10 cursor-pointer z-10 " style={{ background: 'linear-gradient(97.49deg, rgba(138, 168, 248, 0.6) -0.43%, rgba(49, 91, 255, 0.6) 144.53%)'}}>
-            <img src={addToCartIcon} alt="" />
+interface color {
+  firstColor: string;
+  secondColor: string;
+}
+
+const FeaturedPro: FC<FeaturedPro> = (props) => {
+  const { rating, description, price, imgSource, id, indexId } = props;
+  const colors: color[] = [
+    { firstColor: "#FE7831", secondColor: "#FFDEBE" },
+    { firstColor: "#02BE83", secondColor: "#B3F6DC" },
+    { firstColor: "#FF3C78", secondColor: "#FFB2B2" },
+    { firstColor: "#8AA8F8", secondColor: "#315BFF" },
+  ];
+
+  const styles = {
+    background: `linear-gradient(270deg, ${colors[indexId].firstColor} 0%, ${colors[indexId].secondColor} 100%)`,
+  };
+
+  const theRatingStars: JSX.Element[] = [...Array(Math.floor(rating))].map(
+    (el: undefined, index: number) => {
+      return <img src={filledStar} alt="" key={index} />;
+    }
+  );
+
+  const theUnfilledStars: JSX.Element[] = rating < 5 ? [...Array(5 - Math.floor(rating))].map(
+    (el: undefined, index: number) => {
+      return <img src={unFilledStar} alt="" key={index} />;
+    }
+  ): [];
+
+  return (
+    <div className="w-screen h-[20rem] flex flex-col items-center justify-center gap-[1rem]">
+      <div className="w-full h-auto relative flex items-center justify-center">
+        <div
+          className="w-[7rem] h-[7rem] rounded-full absolute top-[-1.5rem] right-[9rem]"
+          style={styles}
+        ></div>
+        <img src={imgSource} alt="" className="h-[10rem] z-0" />
+        <div
+          className="w-[2rem] h-[2rem] rounded-full grid place-items-center absolute bottom-10 cursor-pointer z-10 "
+          style={{
+            background:
+              "linear-gradient(97.49deg, rgba(138, 168, 248, 0.6) -0.43%, rgba(49, 91, 255, 0.6) 144.53%)",
+          }}
+        >
+          <img src={addToCartIcon} alt="" />
         </div>
+      </div>
+      <div className="flex gap-2">{theRatingStars} {theUnfilledStars}</div>
+      <span className="w-[14rem] text-center">
+        {description}
+      </span>
+      <span className="font-bold">$ {price}</span>
     </div>
-    <div className="flex gap-2">
-        <img src={filledStar} alt="" />
-        <img src={filledStar} alt="" />
-        <img src={filledStar} alt="" />
-        <img src={filledStar} alt="" />
-        <img src={unFilledStar} alt="" />
-    </div>
-    <span className="w-[14rem] text-center">Adidas Falcon Shoes for men - 2021 Edition</span>
-    <span className="font-bold">$120.50</span>
-  </div>;
+  );
 };
 
 export default FeaturedPro;
--- src/components/FeaturedPros.tsx
+++ src/components/FeaturedPros.tsx
@@ -2,15 +2,49 @@ import React from "react";
 import FeaturedPro from "./FeaturedPro";
 
 const FeaturedPros = () => {
+  const featuredProducts: FeaturedPro[] = [
+    {
+      rating: 5,
+      description: 'Adidas Falcon Shoes for men - 2021 Edition',
+      price: 110,
+      imgSource: '/server-assets/image 1.png',
+      id: '1',
+      indexId: 0
+    },
+    {
+      rating: 4,
+      description: 'Adidas Falcon Shoes for men - 2021 Edition',
+      price: 100,
+      imgSource: '/server-assets/image 4.png',
+      id: '2',
+      indexId: 1
+    },
+    {
+      rating: 3,
+      description: 'Adidas Falcon Shoes for men - 2021 Edition',
+      price: 130,
+      imgSource: '/server-assets/image 6.png',
+      id: '3',
+      indexId: 2
+    },
+    {
+      rating: 4.5,
+      description: 'Adidas Falcon Shoes for men - 2021 Edition',
+      price: 120,
+      imgSource: '/server-assets/lastImage.png',
+      id: '4',
+      indexId: 3
+    },
+  ];
+
   return (<div className="w-full h-fit bg-white flex flex-col items-center justify-center mt-6 py-6 gap-10  ">
     <div className=" w-auto flex items-between justify-center">
         <h1 className="text-2xl font-bold text-[#0A083A]">Featured Products</h1>
     </div>
     <div className="flex flex-col items-center justify-center gap-6">
-        <FeaturedPro />
-        <FeaturedPro />
-        <FeaturedPro />
-        <FeaturedPro />
+        {featuredProducts.map((product: FeaturedPro) => {
+        const { rating, description, price, imgSource, id, indexId }  = product
+        return <FeaturedPro rating={rating} description={description} price={price} imgSource={imgSource} id={id} indexId={indexId} />})}
     </div>
   </div>);
 };
